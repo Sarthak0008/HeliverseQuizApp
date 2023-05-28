c19c26e9927dbcd072bcbfa80da31997b94dabc9
Sarthak Singh <sarthaksingh.16094@gmail.com>
2023-05-28 12:57:24 +0530

    Update QuizController.java

--- Heliverse/src/main/java/com/heliverse/controller/QuizController.java
+++ Heliverse/src/main/java/com/heliverse/controller/QuizController.java
@@ -16,26 +16,33 @@ import com.heliverse.exception.QuizException;
 import com.heliverse.service.QuizService;
 
 @RestController
-// @RequestMapping("/heliversequizzes")
+@RequestMapping("/heliversequizzes")
 public class QuizController {
 	
 	@Autowired
 	private QuizService quizService;
-	
+
+	// POST controller method to create quiz
 	@PostMapping("/create")
-	public Quiz createQuiz(@RequestBody Quiz quiz) throws QuizException {
+	public Quiz createQuizHandler(@RequestBody Quiz quiz) throws QuizException {
 		return quizService.createQuiz(quiz);
 	}
+	
+	// GET controller method to find active quizzes which is started
 	@GetMapping("/active")
-	public List<Quiz> getActiveQuiz() throws QuizException {
+	public List<Quiz> getActiveQuizHandler() throws QuizException {
 		return quizService.getActiveQuiz();
 	}
+	
+	// controller method to find result by id
 	@GetMapping("/{id}/result")
-	public QuizAnswerDto getQuizResult(@PathVariable Long id) throws QuizException {
+	public QuizAnswerDto getQuizResultHandler(@PathVariable Long id) throws QuizException {
 		return quizService.getQuizResult(id);
 	}
+	
+	// controller method to find all quizzes
 	@GetMapping("/all")
-	public List<Quiz> getAllQuizzes() throws QuizException {
+	public List<Quiz> getAllQuizzesHandler() throws QuizException {
 		return quizService.getAllQuizzes();
 	}
 
